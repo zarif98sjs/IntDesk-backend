501a1d6a6658bb121f71b107e67664457ff4deb0
Md. Zarif Ul Alam <zarif98sjs@gmail.com>
2022-07-11 15:07:14 +0600

    added POST request for a Comment of a Discussion

--- discussions/views.py
+++ discussions/views.py
@@ -1,7 +1,24 @@
-from rest_framework import viewsets
-from discussions.models import Discussion
-from discussions.serializers import DiscussionSerializer
+from gc import get_objects
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from discussions.models import Discussion,Comments
+from discussions.serializers import DiscussionSerializer, CommentSerializer
+from django.shortcuts import get_object_or_404
 
 class DiscussionViewSet(viewsets.ModelViewSet):
     queryset = Discussion.objects.all()
     serializer_class = DiscussionSerializer
+
+    @action(detail=True, methods=['POST'])
+    def comment(self,request,pk):
+        discussion = get_object_or_404(Discussion,pk=pk)
+        data = request.data
+
+        comment_ = Comments.objects.create(
+            comment = data.get('comment'),
+            discussion_id = discussion.id
+        )
+
+        serializer = CommentSerializer(comment_)
+        return Response(serializer.data, status=status.HTTP_200_OK)
